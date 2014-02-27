2af9882347dc0c1d3131ae59f30238cfd8abc76c
Jonathan Inloes <jinloes@Jonathans-MacBook-Pro.local>
2014-02-27 11:32:16 -0800

    marco's feedback

--- api/src/main/java/com/rivermeadow/api/exception/MessageArgumentException.java
+++ api/src/main/java/com/rivermeadow/api/exception/MessageArgumentException.java
@@ -6,6 +6,15 @@ import org.springframework.http.HttpStatus;
 
 /**
  * Designates that an exception will have message arguments.
+ * Example usage:
+ * Message with {@link ErrorCodes} and {@link HttpStatus}
+ * <pre>
+ *     {@code
+ *          new MessageArgumentException(ErrorCodes.JOB_NOT_FOUND, HttpStatus.NOT_FOUND, "123")
+ *     }
+ * </pre>
+ *
+ * would have the response be a 404 and message: No job was found for id: 123.
  */
 public class MessageArgumentException extends ResponseStatusException {
     private final Object[] args;
@@ -22,6 +31,14 @@ public class MessageArgumentException extends ResponseStatusException {
         this(errorCode, HttpStatus.INTERNAL_SERVER_ERROR, t, args);
     }
 
+    /**
+     * Create an exception that will have it's message codes resolved
+     *
+     * @param errorCode error message code to resolve
+     * @param httpStatus http status to return for the exception
+     * @param t throwable to wrap
+     * @param args message arguments
+     */
     public MessageArgumentException(ErrorCodes errorCode, HttpStatus httpStatus, Throwable t,
             Object... args) {
         super(errorCode, httpStatus, t);
--- api/src/main/java/com/rivermeadow/api/model/ErrorMessageDTO.java
+++ api/src/main/java/com/rivermeadow/api/model/ErrorMessageDTO.java
@@ -6,34 +6,11 @@ package com.rivermeadow.api.model;
 public final class ErrorMessageDTO {
     private final String message;
 
-    private ErrorMessageDTO(Builder builder) {
-        this.message = builder.message;
-    }
-
-    public static Builder builder() {
-        return new Builder();
+    private ErrorMessageDTO(String message) {
+        this.message = message;
     }
 
     public String getMessage() {
         return message;
-    }
-
-    /**
-     * A builder for {@linke ErrorMessageDTO}.
-     */
-    public static class Builder {
-        private String message;
-
-        private Builder() {
-        }
-
-        public Builder withMessage(String message) {
-            this.message = message;
-            return this;
-        }
-
-        public ErrorMessageDTO build() {
-            return new ErrorMessageDTO(this);
-        }
     }
 }
